014da64b1ff8925251a00857040c9c7eb098ee4a
Manuel Garcia <manuel_zurdo_10@hotmail.com>
2014-11-11 22:34:10 -0300

    modificacion _header

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110091857) do
+ActiveRecord::Schema.define(version: 20141110121455) do
+
+  create_table "comentarios", force: true do |t|
+    t.integer  "id_user_msj"
+    t.string   "mensaje"
+    t.string   "respuesta"
+    t.integer  "id_user_rta"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"

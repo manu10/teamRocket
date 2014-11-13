db4c93623d64c75099d779a45260eeb7eb1ecd3d
Vazquez F <facu102085@hotmail.com>
2014-11-12 22:41:03 -0300

    Arreglado lo de los comentarios y agregada la posibilidad de realizar nuevos comentarios

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -3,14 +3,12 @@ class Producto < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 
 
+	
 def self.search(query)
-
+	
 	where("titulo like ? or descripcion like ?", "%#{query}%","%#{query}%") 
 	
 end
-
-
-
 
 
 end

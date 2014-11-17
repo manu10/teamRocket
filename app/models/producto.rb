a6f7e8a15d36f722e77f677253cecde3c2bd9180
manu10 <manuel_zurdo_10@hotmail.com>
2014-11-17 11:45:14 -0300

    Arreglos vistas

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -2,9 +2,11 @@ class Producto < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 	validate :expiration_date_cannot_be_in_the_past
+	
+
  
   def expiration_date_cannot_be_in_the_past
-    if vencimiento.present? && vencimiento < Date.today
+    if vencimiento.present? && vencimiento < Date.today+15.days
       errors.add(:vencimiento, "can't be in the past")
     end
   end

73f97b81ea70d2587940f53113e9a3e33c4894fe
manu10 <manuel_zurdo_10@hotmail.com>
2014-11-25 18:48:56 -0300

    Acomodado lo de alta y modificacion de tarjeta de credito

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,5 +1,7 @@
 class CreditCard < ActiveRecord::Base
   belongs_to :user
+
+  
   validates :number,
   		numericality: { only_integer: true },
   		length:{ is:16},
@@ -10,4 +12,13 @@ class CreditCard < ActiveRecord::Base
   		numericality: { only_integer: true },
   		length:{ is:3},
   		allow_blank: true
+
+
+validate :expiration_date_cannot_be_in_the_past
+ 
+  def expiration_date_cannot_be_in_the_past
+    if expireDate < Date.today
+      errors.add(:expireDate, "Su tarjeta esta vencida")
+    end
+  end
 end

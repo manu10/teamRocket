202db270d5b95e656962fa5dcd74e208f14031df
Manuel Garcia <manuel_zurdo_10@hotmail.com>
2014-11-26 18:06:28 -0300

    Intente sin exito solucionar lo del form de la tarjeta de credio al realizar oferta

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,3 +1,18 @@
 class CreditCardsController < ApplicationController
+=begin
+before_action :set_credit_card, only: [:edit, :update, :destroy]
+=end
 
+def update
+	@credit_card = current_user.credit_card
+    @credit_card.update(credit_card_params)
+
+end
+private
+	def set_credit_card 
+      @credit_card = CreditCard.find(current_user.credit_card.id)
+    end
+    def credit_card_params
+      params.require(:domicile).permit(:owner, :expireDate, :securityCode , :number)
+    end
 end

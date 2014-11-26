c049b10d8df7bea09ca48d0099a7d7c20249de78
Manuel Garcia <manuel_zurdo_10@hotmail.com>
2014-11-26 18:35:28 -0300

    Modificando para poner una subasta vencida con una oferta y probar funcionalidad de elegir ganador

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -4,13 +4,14 @@ class Producto < ActiveRecord::Base
   	
 	has_many :comments, dependent: :destroy
 	has_many :oferts, dependent: :destroy
-	validate :expiration_date_cannot_be_in_the_past
+	#validate :expiration_date_cannot_be_in_the_past
 	validates :UrlImage, allow_blank: false, format: {
       with: %r{\.(gif|jpg|jpeg|png)$}i, :multiline => true,
       message: 'URL must point to GIT/JPG/PNG pictures'
   }
 
- 
+=begin
+
   def expiration_date_cannot_be_in_the_past
     if not vencimiento.present?
       errors.add(:vencimiento, "Debe colocar un vencimiento")
@@ -21,7 +22,10 @@ class Producto < ActiveRecord::Base
     	errors.add(:vencimiento, "No puede ser mayor a 30 dias a partir de hoy")
   	end
   end
- 
+
+=end
+
+
 
 	
 def self.search(query)

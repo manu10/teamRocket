0fba97bf12638d58eefec477433305e1a009ca12
E Crudele <ecrudele@users.noreply.github.com>
2014-11-25 23:59:19 -0300

    Implementadas creación, modificación, eliminación de categorías

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,48 @@
 class CategoriesController < ApplicationController
 
+  	before_action :new, only:[:create];
+  	before_action :get_category, only:[:show, :edit, :update, :destroy];
+
+  	def get_category
+		@category = Category.find(params[:id])
+	end
+
+  	def get_category
+		@category = Category.find(params[:id])
+	end
+
 	def index
 		@categories = Category.order("name asc")
 	end
 
 	def show
-		@category = Category.find(params[:id]);
 	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.create(category_params)
+  		redirect_to categories_path
+	end
+
+	def edit
+	end
+
+	def update
+		@category.update(category_params)
+		redirect_to categories_path
+	end
+
+	def destroy
+		@category.destroy
+  		redirect_to categories_path
+	end
+
+	private
+		def category_params
+			params.require(:category).permit([:name])
+		end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
+
+	validates_presence_of :name
 	belongs_to :producto
 	has_many :productos
 	
-
 end

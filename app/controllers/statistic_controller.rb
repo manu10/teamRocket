b8ab4b6aeead2a7b3a84d58c72b23f7fbed5aa97
Manuel Garcia <manuel_zurdo_10@hotmail.com>
2014-12-05 09:52:47 -0300

    Cambie condicion del if de statistic type

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -4,13 +4,13 @@ class StatisticController < ApplicationController
 	@statistic_type=params[:statistic_type]
 	@start_date=Date.new(params[:start_date][:year].to_i,params[:start_date][:month].to_i,params[:start_date][:day].to_i)
 	@end_date=Date.new(params[:end_date][:year].to_i,params[:end_date][:month].to_i,params[:end_date][:day].to_i)
+	
+		
+	if @statistic_type == "1" #Si eligio usuarios
 	@usuarios=User.where("created_at >= :start_date AND created_at <= :end_date",
   {:start_date => @start_date, :end_date => @end_date})
-		
-	if @statistic_type == 1 #Si eligio usuarios
-		@usuarios=User.where("created_at >= :start_date AND created_at <= :end_date",{:start_date => params[:start_date], :end_date => params[:end_date]})
 		render 'statistic/users_statistics'
-	elsif @statistic_type == 2
+	elsif @statistic_type == "2"
 		@recaudacion=1233
 		render 'statistic/income_statistics'
 	end

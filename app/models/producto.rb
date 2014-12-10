4e6fe302a0eb17b3025f24a0a2fc8945274bd1c0
Manuel Garcia <manuel_zurdo_10@hotmail.com>
2014-12-10 17:55:44 -0300

    Acomodar modelo para poder cargar cosas vencidas y poder probar funcionalidad

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -4,23 +4,7 @@ class Producto < ActiveRecord::Base
   	
 	has_many :comments, dependent: :destroy
 	has_many :oferts, dependent: :destroy
-	validate :expiration_date_cannot_be_in_the_past
-	validates :UrlImage, allow_blank: false, format: {
-      with: %r{\.(gif|jpg|jpeg|png)$}i, :multiline => true,
-      message: 'URL must point to GIT/JPG/PNG pictures'
-  }
-
- 
-  def expiration_date_cannot_be_in_the_past
-    if not vencimiento.present?
-      errors.add(:vencimiento, "Debe colocar un vencimiento")
-    elsif vencimiento < Date.today+15.days
-    	errors.add(:vencimiento, "No puede ser menor a 15 dias a partir de hoy")
-    	
-    elsif vencimiento > Date.today+30.days
-    	errors.add(:vencimiento, "No puede ser mayor a 30 dias a partir de hoy")
-  	end
-  end
+	
  
 
 	

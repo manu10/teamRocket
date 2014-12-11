16a9aab974b9f05324e049d846ce5c11921d85bc
Manuel Garcia <manuel_zurdo_10@hotmail.com>
2014-12-10 22:03:25 -0300

    Volviendo a acomodar validaciones y no ver vencidos

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -7,7 +7,7 @@ class ProductosController < ApplicationController
     @productos = Producto.where("vencimiento >= :today ",{:today => Date.today}).search(params[:search]).order("created_at DESC").paginate(page: params[:page], :per_page => 3)
   else
 
-    @productos=Producto.order(sort_column + ' ' + sort_direction).paginate(page: params[:page], :per_page => 5)
+    @productos=Producto.where("vencimiento >= :today ",{:today => Date.today}).order(sort_column + ' ' + sort_direction).paginate(page: params[:page], :per_page => 5)
 
 
 
